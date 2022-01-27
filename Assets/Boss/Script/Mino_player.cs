e9a4326243ee04a4ba86f028d48ade97e0229850
HeoJiye <78946499+HeoJiye@users.noreply.github.com>
2022-01-27 15:17:13 +0900

    스킬 수정 합본

---
+++ Assets/Boss/Script/Mino_player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mino_player : MonoBehaviour
+{
+    //public float Mino_power = 10;
+
+    Animator anim;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // 보스 공격 피격
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "BossAttack") {
+            OnDamaged(other.transform.position);
+            print(other.gameObject.name + " 맞음!");
+
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "BossAttack") {
+            OnDamaged(other.transform.position);
+            print(other.gameObject.name + " 맞음!");
+        }
+    }
+
+    void OnDamaged(Vector2 targetPos)
+    {
+        //gameObject.layer = 13;
+        spriteRenderer.color = new Color(1,0.5f,0.5f,0.8f);
+
+        int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
+        rigid.AddForce(new Vector2(dirc, 1)*3, ForceMode2D.Impulse);
+
+        anim.SetTrigger("isAttacked");
+        Invoke("OffDamaged", 0.5f);
+    }
+    void OffDamaged()
+    {
+        //gameObject.layer = 12;
+        spriteRenderer.color = new Color(1,1,1,1);
+    }
+}
---
+++ Assets/Player/Script/PlayerMove.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMove : MonoBehaviour
+{
+    Animator anim;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+
+    private void Awake() {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    public float jumpPower;
+    public float movePower;
+
+    Vector3 movement;
+
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            moveVelocity = Vector3.left;
+            spriteRenderer.flipX = false;
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            moveVelocity = Vector3.right;
+            spriteRenderer.flipX = true;
+        }
+
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+
+        if (moveVelocity == Vector3.zero) // isWalking false
+            anim.SetBool("isWalking", false);
+        else // true
+            anim.SetBool("isWalking", true);
+
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.C)) {
+
+        rigid.velocity = Vector2.zero;
+
+        Vector2 jumpVelocity = new Vector2(0, jumpPower);
+        rigid.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+        }
+    }
+}
+
+
